7c54e71f22049eaf002b5e19d6a9ac7c5f19a7ef
abhishek9650 <abhishek.mudgal.59@gmail.com>
2018-08-29 18:59:14 +0530

    Disable ims and Timeservice for now

--- wt88047/wt88047-vendor.mk
+++ wt88047/wt88047-vendor.mk
@@ -267,14 +267,14 @@ PRODUCT_COPY_FILES += \
     vendor/wingtech/wt88047/proprietary/vendor/lib/soundfx/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so
 
 PRODUCT_PACKAGES += \
-    ims \
+#    ims \
     imscmlibrary \
     libloc_api_v02 \
     libloc_ds_api \
     libmm-abl \
     libtime_genoff \
     shutdownlistener \
-    TimeService \
+#    TimeService \
     qcrilmsgtunnel \
     qcnvitems \
     qcrilhook
